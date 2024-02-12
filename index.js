364865a046a124703513ef67cfea61cdc745ce25
gee4star <151780860+gee4star@users.noreply.github.com>
2024-02-12 01:38:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
+console.log("Hello")
+document.getElementById('button').addEventListener('click', addInfo)
+document.getElementById('status').addEventListener('click', generateCats)
 
+async function getAllInfo(){
+    await fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+
+async function addInfo(){
+const userInput = document.querySelector('input').value
+await fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: userInput,
+    body: 'bar',
+    userId: 2,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+document.getElementById('deletebutton').addEventListener('click', deleteInfo)
+
+async function deleteInfo(){
+    const userInput = document.getElementById('favAnimal').value
+await fetch('https://jsonplaceholder.typicode.com/posts/2', {
+  method: 'DELETE',
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+
+async function generateCats(){
+    const userInput = document.getElementById('http').value
+    await fetch(`https://http.cat/${userInput}`, {
+    method: 'GET',
+    headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    mode: "no-cors", // no-cors, *cors, same-origin
+})
+const img = document.createElement("img");
+img.src = `https://http.cat/${userInput}`;
+// if (!img) {
+    document.body.appendChild(img);
+    
+// }else{
+//     document.body.replaceChild(img, img); 
+// }
+}
